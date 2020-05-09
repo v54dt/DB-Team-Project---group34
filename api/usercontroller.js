c02e160cf9a97a0998f3d50c1e593a3d5310f8bd
v54dt <vincent71497867@gmail.com>
2020-05-09 22:57:37 +0800

    Update usercontroller.js

--- api/usercontroller.js
+++ api/usercontroller.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const config = require('./config');
 const bodyParser = require('body-parser');
+const async = require('async');
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
@@ -48,11 +49,6 @@ router.get('/search', function (req, res) {
         })
     }
 
-
-
-
-
-
     //district
     if (district !== undefined) {
         var sql2 = "insert into temp_showInfo(`t_UID`) select `artshowUID` from showInfo where location like?";
@@ -76,78 +72,95 @@ router.get('/search', function (req, res) {
     connection.query(sql_final, function (err, result, fields) {
         if (err) throw err;
         //res.status(200).json(result);
-        res.render('../../views/search.ejs',{
-            result : result
+        res.render('../../views/search.ejs', {
+            result: result
         })
     })
-
-    /*
-    var sql_final = "select * from temp_showInfo limit 10;";
-    connection.query(sql_final, function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-
-        res.status(200).json(result);
-
-    })*/
-
     connection.query("drop table temp_artshow;drop table temp_showInfo", function (err, res, fields) {
         if (err) throw err;
     })
-
-
-    /*connection.query(`${sql} limit 2;`, function (err, result, fields) {
-       
-        
-        res.status(200).json(result);
+    /*
+    connection.query(`${sql} limit 2;`, function (err, result, fields) {
     })
-
-    /*var sql = "select * from showInfo where location like  ? limit 2";
-    connection.query(sql, [t + '%'], function (error, result) {
-        console.log(result);
-        res.status(200).json(result);
-    })
-*/
-    //console.log(req);
-    //console.log(req.query.category)
-
-
+    */
 })
 
+router.get('/info/:uid', function (req, res) {
 
-router.get('/users/:name', function (req, res) {
-    res.send(render('result_page.html', {
-        name: req.params.name
-    }));
+    var return_json = {
+        title: "",
+        location: "",
+        locationName: "",
+        category: "",
+        showInfo: [],
+        showUnit: "",
+        descriptionFilterHtml: "",
+        discountInfo: "",
+        price: "",
+        onSales: "",
+        masterUnit: [],
+        imageUrl: "",
+        webSales: "",
+        comment: "",
+        sourceWebName: "",
+        startDate: "",
+        endDate: "",
+    };
+
+    async.parallel([
+        function (finish) {
+
+            var sql = "select * from artshow where `UID` like ?;";
+            connection.query(sql, req.params.uid, function (error, result) {
+                //console.log(result);
+                return_json.title = result[0].title;
+                return_json.category = result[0].category;
+                return_json.showUnit = result[0].showUnit;
+                return_json.descriptionFilterHtml = result[0].descriptionFilterHtml;
+                return_json.onSales = result[0].onSales;
+                return_json.masterUnit = result[0].masterUnit;
+                return_json.imageUrl = result[0].imageUrl;
+                return_json.webSales = result[0].webSales;
+                return_json.comment = result[0].comment;
+                return_json.sourceWebName = result[0].sourceWebName;
+                return_json.startDate = result[0].startDate;
+                return_json.endDate = result[0].endDate;
+
+                finish(error);
+
+            })
+        },
+        function (finish) {
+
+            var sql_showInfo = "select * from showInfo where `artshowUID` like ?;";
+            connection.query(sql_showInfo, req.params.uid, function (error, result) {
+
+
+                for (var i = 0; i < result.length; i++) {
+                    return_json.showInfo.push({
+                        time: result[i].time,
+                        location: result[i].location,
+                        locationName: result[i].locationName,
+                        onSales: result[i].onSales,
+                        latitude: result[i].latitude,
+                        longtitude: result[i].longtitude,
+                        endTime: result[i].endTime
+                    })
+                }
+                finish(error);
+            })
+
+        },
+    ], function (errs, results) {
+        if (errs) throw errs;
+
+        //res.status(200).json(return_json);
+        res.render('../../views/info.ejc',{
+            result: return_json
+        })
+    })
+
 })
-
-
-/*rows.forEach( (row) => {
-  console.log(`${row.name} lives in ${row.city}`);
-});*/
-
-module.exports = router;
-
-/*
-select *
-    from artshow, showInfo
-where locat
-*/
-
-/*
-title
-category
-折扣
-price
-Starttime 
-endtime
-descriptionHTML
-location
-locationName
-*/
-
-
-
 
 /*
 info page
@@ -156,7 +169,7 @@ title
 location
 locationName
 category
-showunit
+showUnit
 descriptionFilter
 discountinfo
 price
@@ -172,6 +185,34 @@ starttime : ["2020/09/26 19:30:00","2020/09/26 19:30:00","2020/05/17 09:30:00"]
 endtime : ["2020/09/26 21:30:00","2020/09/26 21:30:00","2020/09/26 21:30:00"]
 
 */
-/* 
-start_time ~ end_time => Range
+
+
+
+
+/*rows.forEach( (row) => {
+  console.log(`${row.name} lives in ${row.city}`);
+});*/
+
+module.exports = router;
+
+/*
+select *
+    from artshow, showInfo
+where locat
 */
+
+/*
+大綱
+title
+category
+折扣
+price
+Starttime
+endtime
+descriptionHTML
+location
+locationName
+*/
+
+
+
