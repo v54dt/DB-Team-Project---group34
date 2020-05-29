64de67dc94b8a6a0e18527d0e1dd80ebd454705f
james2323123 <james2323123@gmail.com>
2020-05-30 01:11:17 +0800

    update location table

--- create_location_table.sql
+++ create_location_table.sql
@@ -1,7 +1,8 @@
 drop table if exists locations;
 create table locations(
+	UID int primary key not null,
 	city_id tinyint not null,
-	location_name text primary key not null
+	location_name text
 );
 
 load data local infile './locations.csv'
