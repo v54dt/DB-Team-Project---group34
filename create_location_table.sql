2238fcbf1bb3e15cc31973659e6f0392aef57a53
Ubuntu <ubuntu@ip-172-31-89-217.ec2.internal>
2020-06-06 17:06:40 +0000

    update create_location_table.sql

--- create_location_table.sql
+++ create_location_table.sql
@@ -4,3 +4,10 @@ create table locations(
 	city_id tinyint not null,
 	location_name text
 );
+
+load data local infile './locations.csv'
+into table locations
+character set utf8mb4
+fields terminated by ','
+lines terminated by '\n'
+ignore 1 rows;
