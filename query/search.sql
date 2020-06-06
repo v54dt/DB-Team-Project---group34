3ac9631138974ac376b996ad12f6b42f46f2fd97
james2323123 <james2323123@gmail.com>
2020-06-06 21:54:30 +0800

    multiple city at once prototype

--- query/search.sql
+++ query/search.sql
@@ -1,27 +1,52 @@
 drop procedure if exists main;
+drop procedure if exists cat_city_locations;
 
 set @inputStartDate = "2019-09-01";
 set @inputEndDate = "2020-03-20";
-set @inputCityID = 0;
+set @inputCityID = "1,2,3,4";
 set @inputCategoryID = 1;
 set @inputIsFree = 'Y';
+
+
+delimiter //
+create procedure cat_city_locations()
+begin
+	declare i int default 1;
+	declare cnt int default 0;
+	declare location_cur cursor for
+		select location_name
+		from locations
+		where city_id = @curr_cityid
+	;
+	open location_cur;
+	select count(UID) into cnt from locations where city_id = @inputCityID;
+	
+	while i<cnt do
+		set @query = concat(@query, " or ");
+		fetch location_cur into v_location_name;
+		set @query = concat(@query, " instr(showInfo.location, \"", v_location_name, "\" collate utf8mb4_unicode_ci) ");
+		set i=i+1;
+	end while;
+	close location_cur;
+	
+
+end //
+delimiter ;
 
 delimiter //
 create procedure main()
 begin 
-	declare cnt int default 0;
 	declare i int default 1;
+	declare cnt int default 0;
 	declare v_location_name text;
-	declare location_cur cursor for
-		select location_name
-		from locations
-		where city_id = @inputCityID
-	;
+	select length(@inputCityID)-length(replace(@inputCityID,',',''))+1 into cnt;
 	set @query = "
-	select artshow.UID, showInfo.time, showInfo.location, artshow.category
+	select artshow.UID, showInfo.time, showInfo.location, artshow.category, showInfo.onsales
 	from artshow, showInfo
 	where artshow.UID = showInfo.artshowUID
 	";
+	set @curr_cityid = NULL;
+	
 	if not isnull(@inputStartDate) then
 		set @query = concat(@query, " and showInfo.time >= @inputStartDate");
 	end if;
@@ -30,18 +55,13 @@ begin
 	end if;
 	if not isnull(@inputCityID) then
 		
-		open location_cur;
-		select count(UID) into cnt from locations where city_id = @inputCityID;
 		set @query = concat(@query, " and ( false ");
-		while i<cnt do
-			set @query = concat(@query, " or ");
-			fetch location_cur into v_location_name;
-			set @query = concat(@query, " instr(showInfo.location, \"", v_location_name, "\" collate utf8mb4_unicode_ci) ");
-			set i=i+1;
+		while i <= cnt do
+			select substring_index(substring_index("1,2,3,4",',',i),',',-1) into @curr_cityid;
+			call cat_city_locations();
 		end while;
-		close location_cur;
-		set @query = concat(@query, ")");
 		
+		set @query = concat(@query, ")");
 		
 	end if;	
 	if not isnull(@inputCategoryID) then
