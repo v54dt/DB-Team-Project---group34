fd19b24b7ca8740ae12403ef9a33c30d441f027d
james2323123 <james2323123@gmail.com>
2020-06-11 17:16:24 +0800

    update location table and countByLocation

--- query/currMonthCountByLocation.sql
+++ query/currMonthCountByLocation.sql
@@ -1,8 +1,35 @@
 set @currMonth = month(curdate());
 
-#set inputs before calling main!
-#set @inputStartDate = "2019-03-20";
-#set @inputEndDate = "2020-12-31";
-#set @inputCityID = "0,1,2,3,4";
-#set @inputCategoryID = "4,5,6";
-#set @inputIsFree = 'Y';
+
+
+drop procedure if exists currMonthCountByLocation;
+delimiter //
+create procedure currMonthCountByLocation()
+begin
+	
+	declare curr_city_id int default 0;
+	declare max_city_id int default 0;
+	
+	set @o = "";
+	set @inputStartDate = concat(substring_index(curdate(),'-',2),"-01");
+	set @inputEndDate = last_day(curdate());
+	set @inputCategoryID = NULL;
+	set @inputIsFree = NULL;
+	
+	select max(city_id) into max_city_id from locations;
+	while curr_city_id<=max_city_id do
+	
+		set @inputCityID = cast(curr_city_id as char);
+		call main_noexec();
+		set @s1 = concat(
+		"select count(*) into @cnt from (",
+		@query,") as t"
+		);
+		prepare stmt1 from @s1;
+		execute stmt1;
+		set @o = concat('"', curr_city_id, '",', '"', @cnt, '"\n')
+		
+	end while;
+	select @o into outfile '/var/lib/mysql-files/currMonthCountByLocation.csv';
+end //
+delimiter ;
