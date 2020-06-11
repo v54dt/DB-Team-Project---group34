2890149538a1a84c06658bcc436e7ab88543af95
james2323123 <james2323123@gmail.com>
2020-06-11 15:49:25 +0800

    update currMonth search

--- query/currMonthCountByCategory.sql
+++ query/currMonthCountByCategory.sql
@@ -1,8 +1,21 @@
 set @currMonth = month(curdate());
+create table temp(
+	category int not null,
+	categoryCount int
+);
 
+insert into temp
 select artshow.category as category,count(*) as categoryCount
 from artshow, showInfo
 where artshow.UID = showInfo.artshowUID
 and month(showInfo.time) = @currMonth
 group by artshow.category
 ;
+
+select * from temp
+into outfile '/var/lib/mysql-files/currMonthCountByCategory.csv'
+fields terminated by ','
+enclosed by '\"'
+lines terminated by '\n';
+
+drop table temp;
--- query/currMonthCountByLocation.sql
+++ query/currMonthCountByLocation.sql
@@ -1,2 +1,8 @@
 set @currMonth = month(curdate());
 
+#set inputs before calling main!
+#set @inputStartDate = "2019-03-20";
+#set @inputEndDate = "2020-12-31";
+#set @inputCityID = "0,1,2,3,4";
+#set @inputCategoryID = "4,5,6";
+#set @inputIsFree = 'Y';
