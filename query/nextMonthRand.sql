21b3785e398606c08313f6de76a1fb7af2d77da3
james2323123 <james2323123@gmail.com>
2020-06-11 17:57:03 +0800

    update nextMonthRand

--- query/nextMonthRand.sql
+++ query/nextMonthRand.sql
@@ -24,8 +24,8 @@ call getNextMonth();
 select *
 from artshow, showInfo
 where artshow.UID = showInfo.artshowUID
-and month(showInfo.time) = @nextMonth;
-and year(showInfo.time) = @nextMonthYear;
+and month(showInfo.time) = @nextMonth
+and year(showInfo.time) = @nextMonthYear
 order by rand()
 limit 1
 ;
